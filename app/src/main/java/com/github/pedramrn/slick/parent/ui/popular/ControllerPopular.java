df2697895e6bc19b5ee597a0b576f04ba4b3c22b
Pedram <pedramrn@gmail.com>
2017-04-18 01:31:02 +0430

    Revert: Buggy Stuff.

--- app/src/main/java/com/github/pedramrn/slick/parent/ui/popular/ControllerPopular.java
+++ app/src/main/java/com/github/pedramrn/slick/parent/ui/popular/ControllerPopular.java
@@ -10,16 +10,13 @@ import android.widget.CompoundButton;
 import com.bluelinelabs.conductor.Controller;
 import com.github.pedramrn.slick.parent.App;
 import com.github.pedramrn.slick.parent.R;
-import com.github.pedramrn.slick.parent.datasource.database.UserEntity;
+import com.github.pedramrn.slick.parent.domain.model.User;
+import com.github.pedramrn.slick.parent.ui.upcoming.RepositoryUserImpl;
 
 import javax.inject.Inject;
 
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
-import io.requery.Persistable;
-import io.requery.reactivex.ReactiveEntityStore;
-import io.requery.reactivex.ReactiveResult;
 
 /**
  * @author : Pedramrn@gmail.com
@@ -30,48 +27,27 @@ public class ControllerPopular extends Controller {
     private static final String TAG = ControllerPopular.class.getSimpleName();
 
     @Inject
-    ReactiveEntityStore<Persistable> entityStore;
-    private ReactiveResult<UserEntity> userEntities;
+    RepositoryUserImpl repositoryUser;
 
     @NonNull
     @Override
     protected View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container) {
         App.getMainComponent(getRouter()).inject(this);
         final View view = inflater.inflate(R.layout.controller_popular, container, false);
-        final UserEntity userEntity = entityStore.select(UserEntity.class)
-                .where(UserEntity.NAME.equal("joe"))
-                .get().firstOrNull();
+        final User user = repositoryUser.get("joe").take(1).blockingFirst();
 
         final CheckBox checkBox = (CheckBox) view.findViewById(R.id.checkBox_awesome);
-        checkBox.setChecked(userEntity.isAwesome());
+        checkBox.setChecked(user.isAwesome());
         checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
 
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                entityStore.update(UserEntity.class)
-                        .set(UserEntity.AWESOME, isChecked)
-                        .where(UserEntity.NAME.equal("joe"))
-                        .get()
-                        .single()
+                repositoryUser.update(user.getBio(), isChecked)
                         .subscribeOn(Schedulers.single())
                         .observeOn(AndroidSchedulers.mainThread())
                         .subscribe();
             }
         });
-
-        entityStore.select(UserEntity.class)
-                .where(UserEntity.NAME.equal("joe"))
-                .get()
-                .observableResult()
-                .subscribe(new Consumer<ReactiveResult<UserEntity>>() {
-                    @Override
-                    public void accept(@io.reactivex.annotations.NonNull ReactiveResult<UserEntity> userEntities) throws Exception {
-                        if (ControllerPopular.this.userEntities != null) {
-                            System.out.println(ControllerPopular.this.userEntities.first() == userEntities.first());
-                        }
-                        ControllerPopular.this.userEntities = userEntities;
-                    }
-                });
 
         return view;
     }
