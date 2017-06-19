5d916993b146c7373e5f80ef4be3d36c9645a2fa
Pedram <pedramrn@gmail.com>
2017-06-19 21:58:24 +0430

    Removes stupid stuff.

--- app/src/main/java/com/github/pedramrn/slick/parent/ui/popular/ControllerPopular.java
+++ app/src/main/java/com/github/pedramrn/slick/parent/ui/popular/ControllerPopular.java
@@ -4,26 +4,15 @@ import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
 
 import com.bluelinelabs.conductor.Controller;
-import com.github.pedramrn.slick.parent.App;
 import com.github.pedramrn.slick.parent.R;
-import com.github.pedramrn.slick.parent.domain.model.User;
 import com.github.slick.Presenter;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
 
-import io.reactivex.CompletableSource;
-import io.reactivex.functions.Function;
-
-import static android.widget.CompoundButton.OnCheckedChangeListener;
 import static com.github.pedramrn.slick.parent.App.componentMain;
-import static com.github.pedramrn.slick.parent.R.id;
-import static com.github.pedramrn.slick.parent.R.id.checkBox_data_binding;
-import static com.github.pedramrn.slick.parent.R.layout;
 import static com.github.pedramrn.slick.parent.R.layout.controller_popular;
 import static com.github.pedramrn.slick.parent.ui.popular.ControllerPopular_Slick.bind;
 
@@ -48,20 +37,6 @@ public class ControllerPopular extends Controller implements ViewPopular {
     protected View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container) {
         componentMain().inject(this);
         bind(this);
-        final View view = inflater.inflate(controller_popular, container, false);
-
-        final CheckBox checkBoxDataBinding = (CheckBox) view.findViewById(checkBox_data_binding);
-        checkBoxDataBinding.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, final boolean isChecked) {
-                presenter.getUser("joe").take(1).flatMapCompletable(new Function<User, CompletableSource>() {
-                    @Override
-                    public CompletableSource apply(@io.reactivex.annotations.NonNull User user) throws Exception {
-                        return presenter.updateUser("change from 3rd page", isChecked).toCompletable();
-                    }
-                }).subscribe();
-            }
-        });
-        return view;
+        return inflater.inflate(R.layout.controller_popular, container, false);
     }
 }
--- app/src/main/java/com/github/pedramrn/slick/parent/ui/upcoming/ControllerUpComing.java
+++ app/src/main/java/com/github/pedramrn/slick/parent/ui/upcoming/ControllerUpComing.java
@@ -4,10 +4,8 @@ import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CompoundButton;
 
 import com.bluelinelabs.conductor.Controller;
-import com.github.pedramrn.slick.parent.App;
 import com.github.pedramrn.slick.parent.databinding.ControllerUpComingBinding;
 import com.github.slick.Presenter;
 
@@ -17,7 +15,6 @@ import javax.inject.Provider;
 import io.requery.Persistable;
 import io.requery.reactivex.ReactiveEntityStore;
 
-import static android.widget.CompoundButton.OnCheckedChangeListener;
 import static com.github.pedramrn.slick.parent.App.componentMain;
 import static com.github.pedramrn.slick.parent.databinding.ControllerUpComingBinding.inflate;
 import static com.github.pedramrn.slick.parent.ui.upcoming.ControllerUpComing_Slick.bind;
@@ -35,7 +32,7 @@ public class ControllerUpComing extends Controller implements ViewUpComing {
     @Presenter
     PresenterUpComing presenter;
 
-        ViewModelUpComing viewModelUpComing;
+    ViewModelUpComing viewModelUpComing;
     @Inject
     ReactiveEntityStore<Persistable> entityStore;
 
@@ -45,20 +42,12 @@ public class ControllerUpComing extends Controller implements ViewUpComing {
         componentMain().inject(this);
         bind(this);
 
-        if (viewModelUpComing == null) {
+        /*if (viewModelUpComing == null) {
             viewModelUpComing = new ViewModelUpComing(presenter);
-        }
+        }*/
 
 
         final ControllerUpComingBinding binding = inflate(inflater, container, false);
-        binding.setViewModelUser(viewModelUpComing);
-
-        binding.checkBoxShouldBeSame.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                presenter.updateUser("from checkbox up there ^", isChecked).toCompletable().subscribe();
-            }
-        });
 
         return binding.getRoot();
     }
@@ -66,6 +55,6 @@ public class ControllerUpComing extends Controller implements ViewUpComing {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-//        viewModelUpComing.onDestroy();
+        //        viewModelUpComing.onDestroy();
     }
 }
