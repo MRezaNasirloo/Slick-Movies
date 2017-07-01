9b2169bbe11593176867301dfdc678d341e91cb6
Pedram <pedramrn@gmail.com>
2017-07-01 23:30:29 +0430

    Fixes margin.

--- app/src/main/java/com/github/pedramrn/slick/parent/ui/details/ItemDecorationSideMargin.java
+++ app/src/main/java/com/github/pedramrn/slick/parent/ui/details/ItemDecorationSideMargin.java
@@ -20,13 +20,13 @@ public class ItemDecorationSideMargin extends RecyclerView.ItemDecoration {
     @Override
     public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
 
-        /*if (parent.getPaddingLeft() != halfSpace) {
+        if (parent.getPaddingLeft() != halfSpace) {
             parent.setPadding(halfSpace, halfSpace, halfSpace, halfSpace);
             parent.setClipToPadding(false);
-        }*/
+        }
 
-        // outRect.top = halfSpace;
-        // outRect.bottom = halfSpace;
+        outRect.top = halfSpace;
+        outRect.bottom = halfSpace;
         outRect.left = halfSpace;
         outRect.right = halfSpace;
     }
