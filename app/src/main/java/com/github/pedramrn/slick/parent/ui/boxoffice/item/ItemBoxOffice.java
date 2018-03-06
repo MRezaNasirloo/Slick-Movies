48ad5b2bbe073f2c8fcee0ded556beaf20e12cbd
Pedram <pedramrn@gmail.com>
2018-03-06 22:47:55 +0330

    Adds format to revenue k m b $

--- app/src/main/java/com/github/pedramrn/slick/parent/ui/boxoffice/item/ItemBoxOffice.java
+++ app/src/main/java/com/github/pedramrn/slick/parent/ui/boxoffice/item/ItemBoxOffice.java
@@ -12,6 +12,8 @@ import com.github.pedramrn.slick.parent.ui.home.item.RemovableOnError;
 import com.github.pedramrn.slick.parent.ui.list.OnItemAction;
 import com.xwray.groupie.Item;
 
+import java.util.Locale;
+
 /**
  * @author : Pedramrn@gmail.com
  *         Created on: 2017-11-16
@@ -35,11 +37,18 @@ public class ItemBoxOffice extends Item<RowBoxOfficeBinding> implements Removabl
 
     @Override
     public void bind(RowBoxOfficeBinding holder, int position) {
-        if (movie.revenue() != null) holder.revenue.setText(String.format("%s $", movie.revenue()));
+        if (movie.revenue() != null) holder.revenue.setText(String.format("%s $", withSuffix(movie.revenue())));
         holder.title.setText(movie.title());
         holder.rank.setText(movie.rank(position));
         holder.poster.load(movie.thumbnailTinyPoster(), movie.thumbnailPoster());
         holder.poster.setTransitionName(transitionName);
+    }
+
+    private static String withSuffix(@Nullable Long amount) {
+        if (amount == null) return "n/a";
+        if (amount < 1000) return "" + amount;
+        int exp = (int) (Math.log(amount) / Math.log(1000));
+        return String.format(Locale.ENGLISH, "%.1f %c", amount / Math.pow(1000, exp), "kmBTPE".charAt(exp - 1));
     }
 
     @Override
